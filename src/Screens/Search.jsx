8cad058714f078b4f80fb7433c4434f7889504ac
Yingying <92928939+yingyinghao@users.noreply.github.com>
2023-04-04 17:01:32 -0400

    update search component

--- src/Screens/Search.jsx
+++ src/Screens/Search.jsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [courses, setCourses] = useState([
+    "JavaScript",
+    "React",
+    "Node.js",
+    "Python",
+    "HTML",
+    "CSS",
+    "MongoDB",
+    "Express",
+  ]);
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCourses = courses.filter((course) =>
+    course.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <>
       <h1>Search</h1>
-
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-        voluptates, quod, quia, quae quas voluptatibus voluptatem quibusdam
-        voluptatum quidem quos nemo. Quisquam, quae. Quisquam, quae. Quisquam,
-        quae. Quisquam, quae.
-      </p>
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: "20px"
-      }}>
+      <br />
+      <br />
+      <br />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
         <input
-          type="text"
+          type="search"
           placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
           style={{
             padding: "10px",
             borderRadius: "5px",
             border: "1px solid #ccc",
             marginRight: "10px",
             width: "200px",
-            fontSize: "16px"
+            fontSize: "16px",
           }}
         />
         <button
@@ -37,12 +57,19 @@ function Search() {
             color: "white",
             fontSize: "16px",
             border: "none",
-            cursor: "pointer"
+            cursor: "pointer",
           }}
         >
           Search
         </button>
       </div>
+      <br />
+      <card>
+        <h3>Courses: </h3>
+        {filteredCourses.map((course, index) => (
+          <div key={index}>{course}</div>
+        ))}
+      </card>
     </>
   );
 }
