d4f569f42e88ce71ee3766b4b3ddc63840fa2aaa
Aman Patel <aman.180410107062@gmail.com>
2023-04-08 19:55:29 -0400

    enable sign-in with google

--- src/Screens/SignUp.jsx
+++ src/Screens/SignUp.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import {Link, useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 // import { auth, signUp, db } from "../firebase";
-import {auth, signUp} from "../firebase";
-import {useState} from "react";
+import { auth, signUp } from "../firebase";
+import { useState } from "react";
 // import firebase from "firebase/compat/app";
-import {getFirestore, collection, doc, setDoc} from "firebase/firestore";
+import { getFirestore, collection, doc, setDoc } from "firebase/firestore";
 const db = getFirestore();
 
 function SignUp() {
@@ -28,7 +28,6 @@ function SignUp() {
       .then(async (userCredential) => {
         setError(false);
         const user = userCredential.user;
-        console.log(user);
 
         const userDocRef = collection(db, "users");
 
@@ -60,165 +59,173 @@ function SignUp() {
 
   return (
     <>
-      <div classNameName='mt-20'>
-        <section className='bg-white'>
-          <div className='lg:grid lg:min-h-screen lg:grid-cols-12'>
-            <aside className='relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6'>
+      <div classNameName="mt-20">
+        <section className="bg-white">
+          <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+            <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
               <img
-                alt='Pattern'
-                src='https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80'
-                className='absolute inset-0 h-full w-full object-cover'
+                alt="Pattern"
+                src="https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                className="absolute inset-0 h-full w-full object-cover"
               />
             </aside>
 
             <main
-              aria-label='Main'
-              className='flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6'>
-              <div className='max-w-xl lg:max-w-3xl'>
-                <Link className='block text-blue-600' to='/'>
-                  <span className='sr-only'>Login Page</span>
-                  <span className='self-center text-xl font-semibold whitespace-nowrap'>
+              aria-label="Main"
+              className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:py-12 lg:px-16 xl:col-span-6"
+            >
+              <div className="max-w-xl lg:max-w-3xl">
+                <Link className="block text-blue-600" to="/">
+                  <span className="sr-only">Login Page</span>
+                  <span className="self-center text-xl font-semibold whitespace-nowrap">
                     Learn Lab
                   </span>
                 </Link>
 
-                <h1 className='mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl'>
+                <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
                   Welcome to Learn Lab 💻
                 </h1>
 
-                <p className='mt-4 leading-relaxed text-gray-500'>
+                <p className="mt-4 leading-relaxed text-gray-500">
                   Access your Learn Lab account to start your learning journey.{" "}
                   <br />
                   Sign up and start exploring today!
                 </p>
 
                 <form
-                  action='#'
+                  action="#"
                   onSubmit={handleSubmit}
-                  className='mt-8 grid grid-cols-6 gap-6'>
-                  <div className='col-span-6 sm:col-span-3'>
+                  className="mt-8 grid grid-cols-6 gap-6"
+                >
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for='FirstName'
-                      className='block text-sm font-medium text-gray-700'>
+                      for="FirstName"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       First Name
                     </label>
 
                     <input
-                      id='firstName'
-                      name='firstName'
-                      type='text'
-                      placeholder='John'
+                      id="firstName"
+                      name="firstName"
+                      type="text"
+                      placeholder="John"
                       value={firstName}
                       onChange={(e) => setFirstName(e.target.value)}
-                      className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring'
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring"
                       required
                     />
                   </div>
 
-                  <div className='col-span-6 sm:col-span-3'>
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for='LastName'
-                      className='block text-sm font-medium text-gray-700'>
+                      for="LastName"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Last Name
                     </label>
 
                     <input
-                      id='lastName'
-                      name='lastName'
-                      type='text'
-                      placeholder='Doe'
+                      id="lastName"
+                      name="lastName"
+                      type="text"
+                      placeholder="Doe"
                       value={lastName}
                       onChange={(e) => setLastName(e.target.value)}
-                      className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring'
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring"
                       required
                     />
                   </div>
 
-                  <div className='col-span-6'>
+                  <div className="col-span-6">
                     <label
-                      for='Email'
-                      className='block text-sm font-medium text-gray-700'>
+                      for="Email"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Email
                     </label>
 
                     <input
-                      id='Email'
-                      name='email'
-                      type='email'
-                      placeholder='example@gmail.com'
+                      id="Email"
+                      name="email"
+                      type="email"
+                      placeholder="example@gmail.com"
                       value={email}
                       onChange={(e) => setEmail(e.target.value)}
-                      className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring'
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring"
                       required
                     />
                   </div>
 
-                  <div className='col-span-6 sm:col-span-3'>
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for='Password'
-                      className='block text-sm font-medium text-gray-700'>
+                      for="Password"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Password
                     </label>
 
                     <input
-                      type='password'
-                      id='Password'
-                      name='password'
-                      placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;'
+                      type="password"
+                      id="Password"
+                      name="password"
+                      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
                       value={password}
                       onChange={(e) => setPassword(e.target.value)}
-                      className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring'
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring"
                       required
                     />
                   </div>
 
-                  <div className='col-span-6 sm:col-span-3'>
+                  <div className="col-span-6 sm:col-span-3">
                     <label
-                      for='PasswordConfirmation'
-                      className='block text-sm font-medium text-gray-700'>
+                      for="PasswordConfirmation"
+                      className="block text-sm font-medium text-gray-700"
+                    >
                       Password Confirmation
                     </label>
 
                     <input
-                      type='password'
-                      id='PasswordConfirmation'
-                      name='passwordConfirmation'
-                      placeholder='&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;'
+                      type="password"
+                      id="PasswordConfirmation"
+                      name="passwordConfirmation"
+                      placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
                       value={passwordConfirmation}
                       onChange={(e) => setPasswordConfirmation(e.target.value)}
-                      className='block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring'
+                      className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md focus:outline-none focus:ring"
                       required
                     />
                   </div>
 
-                  <div className='col-span-6 sm:flex sm:items-center sm:gap-4'>
+                  <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
                     {error && <p>{error}</p>}
                     <button
-                      className='inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500'
-                      type='submit'>
+                      className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500"
+                      type="submit"
+                    >
                       Create an account
                     </button>
 
-                    <p className='mt-4 text-sm text-gray-500 sm:mt-0'>
+                    <p className="mt-4 text-sm text-gray-500 sm:mt-0">
                       <span>Already have an account? </span>
-                      <Link to='/login'>
-                        <span className='text-blue-700 underline font-medium'>
+                      <Link to="/login">
+                        <span className="text-blue-700 underline font-medium">
                           Sign In
                         </span>
                       </Link>
                     </p>
                   </div>
                 </form>
-                <div className='mt-6 grid grid-cols-4 gap-2'>
-                  <div className='col-span-4 sm:col-span-2'>
-                    <div className='w-auto py-2'>
-                      <button className='flex items-center p-4 bg-white hover:bg-gray-50 border rounded-lg transition ease-in-out duration-200'>
+                <div className="mt-6 grid grid-cols-4 gap-2">
+                  <div className="col-span-4 sm:col-span-2">
+                    <div className="w-auto py-2">
+                      <button className="flex items-center p-4 bg-white hover:bg-gray-50 border rounded-lg transition ease-in-out duration-200">
                         <img
-                          className='mr-3 w-[20px]'
-                          src='https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA'
-                          alt='google'
+                          className="mr-3 w-[20px]"
+                          src="https://lh3.googleusercontent.com/COxitqgJr1sJnIDe8-jiKhxDx1FrYbtRHKJ9z_hELisAlapwE9LUPh6fcXIfb5vwpbMl4xl9H9TRFPc5NOO8Sb3VSgIBrfRYvW6cUA"
+                          alt="google"
                         />
-                        <span className='font-semibold leading-normal'>
+                        <span className="font-semibold leading-normal">
                           Sign in with Google
                         </span>
                       </button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,27 @@
-import React, {useContext, useEffect, useState} from "react";
-import {Link} from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import icon from "../components/img/icon.png";
-import {AuthContext} from "../context/AuthContext";
-import {useNavigate} from "react-router-dom";
-import {auth} from "../firebase";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
 
 function Header() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  const {currentUser} = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
   useEffect(() => {
     setIsLoggedIn(currentUser ? true : false);
   }, [currentUser]);
 
   const navigate = useNavigate();
-  const {dispatch} = useContext(AuthContext);
+  const { dispatch } = useContext(AuthContext);
 
   const handleLogout = () => {
-    auth
-      .signOut()
+    const auth = getAuth();
+    signOut(auth)
       .then(() => {
-        dispatch({type: "LOGOUT"});
+        dispatch({ type: "LOGOUT" });
         navigate("/");
       })
       .catch((error) => {
@@ -30,65 +30,71 @@ function Header() {
   };
 
   const loggedInNav = (
-    <nav className='bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 mb-36'>
-      <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <div href='/' className='flex items-center'>
-          <img src={icon} alt='logo' className='h-10 w-10' />
-          <span className='self-center text-xl font-semibold whitespace-nowrap'>
+    <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 mb-36">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <div href="/" className="flex items-center">
+          <img src={icon} alt="logo" className="h-10 w-10" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
             Learn Lab
           </span>
         </div>
-        <div className='flex md:order-2'>
+        <div className="flex md:order-2">
           <button
-            type='button'
+            type="button"
             onClick={handleLogout}
-            className='text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-3 md:mr-0'>
+            className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-3 md:mr-0"
+          >
             Logout
           </button>
 
           <button
-            data-collapse-toggle='navbar-sticky'
-            type='button'
-            className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200'
-            aria-controls='navbar-sticky'
-            aria-expanded='false'>
-            <span className='sr-only'>Open main menu</span>
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
             <svg
-              className='w-6 h-6'
-              aria-hidden='true'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'>
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
-                fillRule='evenodd'
-                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-                clipRule='evenodd'></path>
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
             </svg>
           </button>
         </div>
         <div
-          className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
-          id='navbar-sticky'>
-          <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-white'>
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-white">
             <li>
               <Link>
                 <div
-                  className='block py-2 pl-3 pr-4 text-blue-700 bg-blue-200 rounded md:bg-transparent md:text-blue-700 md:p-0'
-                  aria-current='page'>
+                  className="block py-2 pl-3 pr-4 text-blue-700 bg-blue-200 rounded md:bg-transparent md:text-blue-700 md:p-0"
+                  aria-current="page"
+                >
                   Dashboard
                 </div>
               </Link>
             </li>
             <li>
               <Link>
-                <div className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'>
+                <div className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
                   Search Courses
                 </div>
               </Link>
             </li>
             <li>
               <Link>
-                <div className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'>
+                <div className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
                   My Account
                 </div>
               </Link>
@@ -100,81 +106,88 @@ function Header() {
   );
 
   const loggedOutNav = (
-    <nav className='bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 mb-36'>
-      <div className='container flex flex-wrap items-center justify-between mx-auto'>
-        <div href='/' className='flex items-center'>
-          <img src={icon} alt='logo' className='h-10 w-10' />
-          <span className='self-center text-xl font-semibold whitespace-nowrap'>
+    <nav className="bg-white px-2 sm:px-4 py-2.5 fixed w-full z-20 top-0 left-0 border-b border-gray-200 mb-36">
+      <div className="container flex flex-wrap items-center justify-between mx-auto">
+        <div href="/" className="flex items-center">
+          <img src={icon} alt="logo" className="h-10 w-10" />
+          <span className="self-center text-xl font-semibold whitespace-nowrap">
             Learn Lab
           </span>
         </div>
-        <div className='flex md:order-2'>
-          <Link to='/login'>
+        <div className="flex md:order-2">
+          <Link to="/login">
             <button
-              type='button'
-              className='text-blue-600 hover:text-blue-800 font-medium rounded-lg text-md py-2.5 text-center mr-3 md:mr-5'>
+              type="button"
+              className="text-blue-600 hover:text-blue-800 font-medium rounded-lg text-md py-2.5 text-center mr-3 md:mr-5"
+            >
               Log In
             </button>
           </Link>
 
-          <Link to='/signup'>
+          <Link to="/signup">
             <button
-              type='button'
-              className='text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-3 md:mr-0'>
+              type="button"
+              className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-md px-5 py-2.5 text-center mr-3 md:mr-0"
+            >
               Sign Up
             </button>
           </Link>
 
           <button
-            data-collapse-toggle='navbar-sticky'
-            type='button'
-            className='inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200'
-            aria-controls='navbar-sticky'
-            aria-expanded='false'>
-            <span className='sr-only'>Open main menu</span>
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
             <svg
-              className='w-6 h-6'
-              aria-hidden='true'
-              fill='currentColor'
-              viewBox='0 0 20 20'
-              xmlns='http://www.w3.org/2000/svg'>
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path
-                fillRule='evenodd'
-                d='M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
-                clipRule='evenodd'></path>
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                clipRule="evenodd"
+              ></path>
             </svg>
           </button>
         </div>
         <div
-          className='items-center justify-between hidden w-full md:flex md:w-auto md:order-1'
-          id='navbar-sticky'>
-          <ul className='flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-white'>
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-md md:font-medium md:border-0 md:bg-white">
             <li>
               <Link>
                 <div
-                  className='block py-2 pl-3 pr-4 text-blue-700 bg-blue-200 rounded md:bg-transparent md:text-blue-700 md:p-0'
-                  aria-current='page'>
+                  className="block py-2 pl-3 pr-4 text-blue-700 bg-blue-200 rounded md:bg-transparent md:text-blue-700 md:p-0"
+                  aria-current="page"
+                >
                   Home
                 </div>
               </Link>
             </li>
             <li>
               <Link>
-                <div className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'>
+                <div className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
                   About
                 </div>
               </Link>
             </li>
             <li>
               <Link>
-                <div className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'>
+                <div className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
                   Testimonials
                 </div>
               </Link>
             </li>
             <li>
               <Link>
-                <div className='block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0'>
+                <div className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0">
                   Courses
                 </div>
               </Link>
