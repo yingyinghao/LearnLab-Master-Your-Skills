7390249a79c671c09399003bcb82dbe418889d0d
guocong <congguocn@outlook.com>
2023-04-09 23:21:50 -0400

    update the favorite part

--- src/Screens/Dashboard.jsx
+++ src/Screens/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
-import {getFirestore, collection, getDocs} from "firebase/firestore";
+import {getFirestore, collection, getDocs, doc, updateDoc} from "firebase/firestore";
 import Card from "../components/Card";
+import { Link } from "react-router-dom";
 
 const db = getFirestore();
 
@@ -19,14 +20,67 @@ const Dashboard = () => {
     getCourses();
   }, []);
 
+  const handleAddToFavorite = async (courseId) => {
+    try {
+      const courseRef = doc(db, "courses", courseId);
+      await updateDoc(courseRef, { addedToFavorite: true });
+      setCourses((prevState) =>
+        prevState.map((course) =>
+          course.id === courseId ? { ...course, addedToFavorite: true } : course
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleRemoveFromFavorite = async (courseId) => {
+    try {
+      const courseRef = doc(db, "courses", courseId);
+      await updateDoc(courseRef, { addedToFavorite: false });
+      setCourses((prevState) =>
+        prevState.map((course) =>
+          course.id === courseId ? { ...course, addedToFavorite: false } : course
+        )
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='mt-24'>
       <h1>Dashboard</h1>
       {courses.map((courseData, index) => (
         <div key={index}>
           <Card courseData={courseData} />
+          {courseData.addedToFavorite ? (
+            <button-added onClick={() => handleRemoveFromFavorite(courseData.id)}>
+              Added
+            </button-added>
+          ) : (
+            <button-addtofavorite onClick={() => handleAddToFavorite(courseData.id)}>
+              Add to my favorite
+            </button-addtofavorite>
+          )}
+          {/* {courseData.addedToFavorite ? (
+            <button disabled>Added</button>
+          ) : (
+            <button onClick={() => handleAddToFavorite(courseData.id)}>
+              Add to my favorite
+            </button>
+          )} */}
         </div>
       ))}
+            <Link
+            to="/favorite"
+              // target='_blank'
+              className='relative inline-block items-center justify-center p-4 px-5 py-3 overflow-hidden font-medium text-blue-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-blue-500 sm:mt-0'>
+              <span className='absolute inset-0 w-full h-full bg-gradient-to-br from-sky-500 via-violet-600 to-blue-800'></span>
+              <span className='absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-indigo-600 rounded-full opacity-30 group-hover:rotate-90 ease'></span>
+              <span className='relative text-white'>My Favorite</span>
+            </Link>
+      {/* <Link to="/favorite">My Favorite</Link> */}
     </div>
   );
 };
--- src/Screens/Favorite.jsx
+++ src/Screens/Favorite.jsx
@@ -1,67 +1,39 @@
-import courseDataList from "../Data/courseDataList";
 import React, { useState, useEffect } from "react";
-import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { getFirestore, collection, getDocs, where, query, updateDoc} from "firebase/firestore";
 import Card from "../components/Card";
 
 const db = getFirestore();
 
-const getAverageReview = (courseDataList) => {
-  let totalReviews = 0;
-  let numCourses = courseDataList.length;
-
-  for (let course of courseDataList) {
-    totalReviews += parseFloat(course.reviews);
-  }
-
-  let averageReview = totalReviews / numCourses;
-  return averageReview.toFixed(2);
-};
-
-function recalculateAndSortCourses(courses) {
-  courses.forEach((course) => {
-// Sort courses by reviews and reviewers
-// renewScorce = reviewers/(reviewers+600)*reviews + 600/(600+reviewers)*averageReview
-    const reviews = parseFloat(course.reviews);
-    const reviewers = parseInt(course.reviewers, 10);
-    const averageReview = getAverageReview(courseDataList);
-    course.score =
-      (reviewers / (reviewers + 600)) * reviews + (600 / (reviewers + 600)) * averageReview;
-  });
-
-  courses.sort((a, b) => b.score - a.score);
-  return courses;
-}
-
-function FavoriteCourses() {
-  const [courses, setCourses] = useState([]);
+const Favorite = () => {
+  const [favoriteCourses, setFavoriteCourses] = useState([]);
 
   useEffect(() => {
-    const getCourses = async () => {
-      const coursesSnapshot = await getDocs(collection(db, "courses"));
-      const coursesData = coursesSnapshot.docs.map((doc) => {
+    const getFavoriteCourses = async () => {
+      const favoriteCoursesQuery = query(collection(db, "courses"), where("addedToFavorite", "==", true));
+      const favoriteCoursesSnapshot = await getDocs(favoriteCoursesQuery);
+      const favoriteCoursesData = favoriteCoursesSnapshot.docs.map((doc) => {
         return { id: doc.id, ...doc.data() };
       });
-      setCourses(coursesData);
+      setFavoriteCourses(favoriteCoursesData);
     };
 
-    getCourses();
-}, []);
-
-  const sortedCourses = recalculateAndSortCourses(courses);
-  const topThreeCourses = sortedCourses.slice(0, 3);
+    getFavoriteCourses();
+  }, []);
 
   return (
     <div className="mt-24">
-      <h1>Favorite Courses</h1>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        {topThreeCourses.map((course, index) => (
-          <div key={index} style={{ flex: 1, margin: "0 8px" }}>
-            <Card courseData={course} />
+      <h1>My Favorite</h1>
+      {favoriteCourses.length ? (
+        favoriteCourses.map((courseData, index) => (
+          <div key={index}>
+            <Card courseData={courseData} />
           </div>
-        ))}
-      </div>
+        ))
+      ) : (
+        <p>No favorite courses</p>
+      )}
     </div>
   );
-}
+};
 
-export default FavoriteCourses;
+export default Favorite;
